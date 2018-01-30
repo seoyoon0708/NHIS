95d04654ba66bf36f0f81a247313e526cfb21106
sungjaejung <34935152+sungjaejung@users.noreply.github.com>
2018-01-30 18:53:54 +0900

    Update condition_occurrence.sql

--- SQL/NHIStoCDM/condition_occurrence.sql
+++ SQL/NHIStoCDM/condition_occurrence.sql
@@ -1,27 +1,29 @@
 truncate table condition_occurrence;
 
+
 insert /*+append*/ into condition_occurrence(
 	condition_occurrence_id, person_id, condition_concept_id, condition_start_date, condition_end_date, 
 	condition_type_concept_id, stop_reason, provider_id, visit_occurrence_id, condition_source_value, condition_source_concept_id
-	)
-
+	)    
 select 
 	--convert(bigint, convert(varchar, m.master_seq) + convert(varchar, ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc))) as condition_occurrence_id,
-	--ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc) AS rank, m.seq_no,
-	condition_occurrence_id,
+	--ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc) AS rank, m.seq_no,    
+	ROWNUM condition_occurrence_id,
 	m.person_id as person_id,
-	n.concept_id as condition_concept_id,
+	n.SNOMED_CODE as condition_concept_id,
 	TO_DATE(m.recu_fr_dt, 'YYYYMMDD') as condition_start_date,
 	m.visit_end_date as condition_end_date,
 	m.sick_order as condition_type_concept_id,
 	null as stop_reason,
 	null as provider_id,
-	m.key_seq as visit_occurrence_id,
+	m.visit_occurrence_id,
 	m.sick_sym as condition_source_value,
-	null as condition_source_concept_id
+	null as condition_source_concept_id 
 from (
 	select
-		--a.master_seq, a.person_id, a.key_seq, a.seq_no, 
+		--a.master_seq, a.person_id, a.key_seq, a.seq_no,
+        b.person_id,
+        b.visit_occurrence_id,
 		b.recu_fr_dt,
 		case when b.form_cd in ('02', '04', '06', '07', '10', '12') then TO_DATE(b.recu_fr_dt, 'YYYYMMDD')+b.vscn-1
 			when b.form_cd in ('03', '05', '08', '09', '11', '13', '20', '21', 'ZZ') and b.in_pat_cors_type in ('11', '21', '31') then TO_DATE(b.recu_fr_dt, 'YYYYMMDD')+b.vscn-1 
@@ -35,16 +37,14 @@ from (
 		case when b.sub_sick=c.sick_sym then 'Y' else 'N' end as sub_sick_yn
 	from NHIS.NHIS_20T b, NHIS.NHIS_40T c
 --		observation_period d --추가
-	where a.person_id=b.person_id
-	and a.key_seq=b.key_seq
-	and a.key_seq=c.key_seq
-	and a.seq_no=c.seq_no
---	and b.person_id=d.person_id --추가
+	where b.key_seq=c.key_seq	
 --	and TO_DATE(c.recu_fr_dt, 'YYYYMMDD') between d.observation_period_start_date and d.observation_period_end_date
-	) as m, --추가
-	 생성할매핑테이블 as n
-where m.sick_sym=n.kcdcode	
+	)  m, --추가
+	 TS_MAP_ICD2SNOMED n
+where m.sick_sym=n.SNOMED_CODE	
 ;
+
+commit;
 
 
 -- MAPPING TABLE ATC TO RXNORM 
